306789936ace6b633d756db2b2f72f817c3a81b4
NICOLAS GOMEZ CRUZ <nigomezcr@valinor.domain.org>
2020-11-20 12:41:52 -0500

    MPi examples

---
+++ Class19_HPC_III/parallel_simpson_mpi.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+#include <functional>
+#include <omp.h>
+#include <chrono>
+
+using fptr  = double(double);
+
+void print_elapsed(auto start, auto end);
+double f(double x);
+double simpson(fptr fun, double a, double b, int nthreads);
+
+const int N = 1000000;
+const double xmin = 0.0;
+const double xmax = 10.0;
+
+int main(int argc, char **argv)
+{
+    const int NTH = std::atoi(argv[1]);
+    std::cout.precision(15); std::cout.setf(std::ios::scientific);
+
+    auto start  = std::chrono::steady_clock::now();
+    double integral  = simpson(f, xmin, xmax, NTH);
+    auto end  = std::chrono::steady_clock::now();
+    print_elapsed(start, end);
+
+    std::cout << "El valor de la integral con " << NTH << " threads es: "  << integral << '\n';
+
+    return 0;
+}
+
+double f(double x)
+{
+    return std::pow(x,2);
+}
+
+
+
+double simpson(fptr fun, double a, double b, int nthreads)
+{
+
+    const double h = (b-a)/N;
+    double suma = 0;
+
+    double aux1 = 0, aux2 = 0;
+    int nth = omp_get_num_threads();
+    int tid = omp_get_thread_num();
+    int SL = N/nth;
+    
+#pragma omp parallel for num_threads (nthreads) reduction(+:aux1,aux2)
+      for(int ii = 1; ii <= N/2 - 1; ++ii) {
+        double x = a + 2*ii*h;
+        aux1 += fun(x);
+      }
+      for(int ii = 1; ii <= N/2; ++ii) {
+        double x = a + (2*ii-1)*h;
+        aux2 += fun(x);
+      }
+      suma = h*(fun(a) + 2*aux1 + 4*aux2 + fun(b))/3;
+
+    return suma;
+}
+
+void print_elapsed(auto start, auto end)
+{
+  std::cout << "t= " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0 << "\n";
+}
