65ead92be995d7ff94caa8f7d49c0c750fe74f65
nigomezcr@github.com <>
2020-11-20 15:43:26 -0500

    simpson mpi working

--- Class19_HPC_III/simpson_mpi.cpp
+++ Class19_HPC_III/simpson_mpi.cpp
@@ -6,12 +6,10 @@
 
 using fptr  = double(double);
 
-
+void print_elapsed(auto start, auto end);
+void suma(double *data, int NSlocal, int pid, int nproc, double vl);
 double f(double x);
-double simpson(fptr fun, double *data, int ns, int nslocal, int pid, int nproc);
-void print(double *data, int ns, int nslocal, int pid, int nproc);
-void print_local(double *data, int ns, int nslocal, int pid, int nproc);
-
+double simpson(fptr fun, double a, double b, int N);
 
 const int N = 6000000;
 const double xmin = 0.0;
@@ -19,33 +17,31 @@ const double xmax = 100.0;
 
 int main(int argc, char **argv)
 {
+
+  std::cout.precision(15); std::cout.setf(std::ios::scientific);
+    
     MPI_Init(&argc, &argv);
     int pid = 0, nthreads = 0;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Comm_size(MPI_COMM_WORLD, &nthreads);
-
-    const int NSlocal = N/nthreads;
-    const double h = (xmax-xmin)/N;
-    double * array = new double [N] {0.0};
-    double * data = new double [nthreads] {0.0};
-
-    for (int i = 0; int < N; i++)
-    {
-      array[i] = xmin + i*h;
-    }   
-    std::cout.precision(05); std::cout.setf(std::ios::scientific);
-
-
-    double integral  = simpson(f, array, N, NSlocal, nthreads);
-
-
-    std::cout << "tiempo:\t";
-
-
-    std::cout << "valor de la integral:\t" << integral << '\n';
+    
+    double NSlocal = xmax/nthreads;
+    double valorlocal = 0, *data = 0;
+    
+    auto start  = std::chrono::steady_clock::now();
+    valorlocal = simpson(f,NSlocal*pid,NSlocal*(pid+1),N/nthreads);
+    data = &valorlocal;
+    auto end  = std::chrono::steady_clock::now();
+    
+    std::cout << "pid: " << pid << "\n"
+	      << "valorlocal: " << *data << "\n" << "tiempo:\t";
+    print_elapsed(start, end);
+    std::cout << "\n\n";
+    suma(data, NSlocal, pid, nthreads, valorlocal);
 
     MPI_Finalize();
+    
     return 0;
 }
 
@@ -55,60 +51,46 @@ double f(double x)
 }
 
 
-void print(double *data, int ns, int nslocal, int pid, int nproc)
+double simpson(fptr fun, double a, double b, int N)
 {
- int tag = 0;
-  if (0 == pid) {
-    double * aux = new double [nslocal] {0.0};
-    print_local(data, ns, nslocal, pid, nproc);
-    for (int src = 1; src < nproc; ++src) {
-      MPI_Recv(aux, nslocal, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      print_local(aux, ns, nslocal, src, nproc);
-    }
-    delete [] aux;
-  } else {
-    int dest = 0;
-    MPI_Send(data, nslocal, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
-  }
-}
 
-
-void print_local(double *data, int ns, int nslocal, int pid, int nproc)
-{
-  std::cout << "pid: " << pid << "\n";
-  for (int ilocal = 0; ilocal < nslocal; ++ilocal) {
-    std::cout << data[ilocal] << "  ";
-  }
-  std::cout << "\n";
-}
-
-
-
-
-double simpson(fptr fun, double *data, int ns, int nslocal, int pid, int nproc)
-{
-  int tag = 0;
+    const double h = (b-a)/N;
     double suma = 0;
     double aux1 = 0, aux2 = 0;
-
-    sum = std::accumulate(data, data+nslocal, 0.0);
-
-    if(0 == pid){
-      double aux;
-      for(int ii = 1; ii <= N/2-1; ++ii) {
-        MPI_Recv(&aux, 1, MPI_DOUBLE, ii, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        data[ii] = xmin + 2*ii*h;
-        aux1 += fun(data[ii]);
+ 
+      for(int ii = 1; ii <= N/2 - 1; ++ii) {
+        double x = a + 2*ii*h;
+        aux1 += fun(x);
       }
       for(int ii = 1; ii <= N/2; ++ii) {
-        MPI_Recv(&aux, 1, MPI_DOUBLE, ii, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        data[ii] = xmin + (2*ii-1)*h;
-        aux2 += fun(data[ii]);
+        double x = a + (2*ii-1)*h;
+        aux2 += fun(x);
       }
-      suma = h*(fun(a) + 2*aux1 + 4*aux2 + fun(xmax))/3;
-    }  else {
-      int dest = 0;
-      MPI_Send(&suma, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
-    }
+      suma = h*(fun(a) + 2*aux1 + 4*aux2 + fun(b))/3;
+
     return suma;
+}
+
+void print_elapsed(auto start, auto end)
+{
+  std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0 << "\n";
+}
+
+
+void suma(double *data, int NSlocal, int pid, int nproc, double vl)
+{
+  int tag = 0;
+  double sum = 0.0;
+  if (0 == pid) {
+    sum = vl;
+    double aux;
+    for (int src = 1; src < nproc; ++src) {
+      MPI_Recv(&aux, 1, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      sum += aux;
+    }
+    std::cout << "valor de la integral:\t" << sum << '\n';
+  } else {
+    int dest = 0;
+    MPI_Send(data, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+  }
 }
