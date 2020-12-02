6943f3ada72c0fc6e5e6716da2a6677c06e5ef84
NICOLAS GOMEZ CRUZ <nigomezcr@valinor.domain.org>
2020-12-02 12:04:09 -0500

    Added first examples with mpi and hw7 solution

---
+++ Class20_MPI-I/mpi-V1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mpi.h"
+
+int main(int argc, char **argv)
+{
+  MPI_Init(&argc, &argv);
+  int pid = 0, nproc = 0;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+  std::cout << "Hello world from pid: " << pid
+	    << " out of " << nproc << std::endl ;
+
+  MPI_Finalize();
+  return 0;
+}
